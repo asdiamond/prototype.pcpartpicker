0ab800553d119341d44be09cec0cd5b8ebaec758
KevinParker10 <kaparker@miners.utep.edu>
2017-02-15 15:00:11 -0700

    Initial commit

---
+++ app/src/main/java/com/example/kevinparker/pcpartpicker/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.kevinparker.pcpartpicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView cpuTV = (TextView)findViewById(R.id.cpuTV);
+        cpuTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextView cpuCoolerTV = (TextView)findViewById(R.id.cpuCoolerTV);
+        cpuCoolerTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+}
