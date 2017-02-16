90d4bced60cd4a31b1b408eabc9a667e19b45ff5
asdiamond <asdiamond@miners.utep.edu>
2017-02-15 20:52:10 -0700

    Added an overflow menu to the CPUActivity

--- app/src/main/java/com/example/kevinparker/pcpartpicker/CPUActivity.java
+++ app/src/main/java/com/example/kevinparker/pcpartpicker/CPUActivity.java
@@ -1,13 +1,11 @@
 package com.example.kevinparker.pcpartpicker;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class CPUActivity extends AppCompatActivity {
 
@@ -19,5 +17,28 @@ public class CPUActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Choose a CPU");//sets title on activity.
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);//this enables the back button in the toolbar. IMPORTANT, in order for this to work
         // you must specify android:parentActivityName in the android manifest under the activity you are referencing.
+    }
+
+    //creates option menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.cpu_activity_menu, menu);//the menu being created
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //callback for menu item selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.filter_menu_item:
+                Toast.makeText(this, "Filter Item Selected", Toast.LENGTH_SHORT).show();
+                //TODO deliver event to a fragment for displaying the filtered items
+                break;
+            case R.id.temp:
+                Toast.makeText(this, "Temp item selected", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
