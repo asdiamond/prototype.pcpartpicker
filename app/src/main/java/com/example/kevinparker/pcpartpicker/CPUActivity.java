1fa5a27c43059adc49c1e5ce97efc6b1a550e308
asdiamond <asdiamond@miners.utep.edu>
2017-02-16 21:45:21 -0700

    Dialog Testing

--- app/src/main/java/com/example/kevinparker/pcpartpicker/CPUActivity.java
+++ app/src/main/java/com/example/kevinparker/pcpartpicker/CPUActivity.java
@@ -1,13 +1,22 @@
 package com.example.kevinparker.pcpartpicker;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CPUActivity extends AppCompatActivity {
+    Dialog yourDialog;
+    TextView tv;
+    String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,22 @@ public class CPUActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Choose a CPU");//sets title on activity.
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);//this enables the back button in the toolbar. IMPORTANT, in order for this to work
         // you must specify android:parentActivityName in the android manifest under the activity you are referencing.
+
+        tv = (TextView)findViewById(R.id.textView);
+        yourDialog = new Dialog(this);
+        LayoutInflater inflater = (LayoutInflater)this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.my_custom_dialog, (ViewGroup)findViewById(R.id.customDialog_root_element));
+        yourDialog.setContentView(layout);
+
+        Button b = (Button)yourDialog.findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(CPUActivity.this, "50", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
     //creates option menu
@@ -34,6 +59,9 @@ public class CPUActivity extends AppCompatActivity {
             case R.id.filter_menu_item:
                 Toast.makeText(this, "Filter Item Selected", Toast.LENGTH_SHORT).show();
                 //TODO deliver event to a fragment for displaying the filtered items
+
+                //for the custom dialog
+                yourDialog.show();
                 break;
             case R.id.temp:
                 Toast.makeText(this, "Temp item selected", Toast.LENGTH_SHORT).show();
@@ -41,4 +69,6 @@ public class CPUActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
