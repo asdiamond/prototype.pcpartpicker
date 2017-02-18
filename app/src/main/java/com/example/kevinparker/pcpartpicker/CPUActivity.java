413fbbe3306b87271233a1d5bcec1da3f00fcf03
asdiamond <asdiamond@miners.utep.edu>
2017-02-18 15:02:05 -0700

    Added toast that displays text of lowest price editText in dialog. All in cpu activity

--- app/src/main/java/com/example/kevinparker/pcpartpicker/CPUActivity.java
+++ app/src/main/java/com/example/kevinparker/pcpartpicker/CPUActivity.java
@@ -3,7 +3,6 @@ package com.example.kevinparker.pcpartpicker;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,51 +10,21 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class CPUActivity extends AppCompatActivity {
-    Dialog yourDialog;
+    Dialog filterDialog;
     TextView tv;
-    String str;
-    String lowestPrice;
-    EditText lowestPriceet;
-    TextView lowestpricedisplay;
+    String lowestPriceStr;
+    EditText lowestPriceET;
+    TextView lowestPriceDisplyTV;
+    ImageView dismissBTN;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_cpu);
-
-        getSupportActionBar().setTitle("Choose a CPU");//sets title on activity.
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//this enables the back button in the toolbar. IMPORTANT, in order for this to work
-        // you must specify android:parentActivityName in the android manifest under the activity you are referencing.
-
-
-        lowestpricedisplay = (TextView)findViewById(R.id.lowestprice);
-
-        tv = (TextView)findViewById(R.id.textView);
-        yourDialog = new Dialog(this);
-        LayoutInflater inflater = (LayoutInflater)this.getSystemService(LAYOUT_INFLATER_SERVICE);
-        View layout = inflater.inflate(R.layout.my_custom_dialog, (ViewGroup)findViewById(R.id.customDialog_root_element));
-        yourDialog.setContentView(layout);
-        yourDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialogInterface) {
-//                lowestPriceet = (EditText)yourDialog.findViewById(R.id.lowestprice);
-//                lowestPrice = lowestPriceet.getText().toString();
-
-                Toast.makeText(CPUActivity.this, "Dismissed", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-
-
+    public void setLowestPriceDisplyTV(String in) {
+        this.lowestPriceDisplyTV.setText(in);
     }
 
     //creates option menu
@@ -69,32 +38,54 @@ public class CPUActivity extends AppCompatActivity {
     //callback for menu item selected
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         switch (item.getItemId()) {
             case R.id.filter_menu_item:
-                Toast.makeText(this, "Filter Item Selected", Toast.LENGTH_SHORT).show();
-                //TODO deliver event to a fragment for displaying the filtered items
-
-                //for the custom dialog
-                yourDialog.show();
-
-                ImageView dismissButton = (ImageView)yourDialog.findViewById(R.id.closeButton);
-                dismissButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-//                        lowestPrice = lowestPriceet.getText().toString();
-                        lowestPrice = "anything";
-                        yourDialog.dismiss();
-                        lowestpricedisplay.setText(lowestPrice);
-                        Toast.makeText(CPUActivity.this, "Dismissed", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
+                filterDialog.show();//shows the dialogue for filtering items
                 break;
             case R.id.temp:
                 Toast.makeText(this, "Temp item selected", Toast.LENGTH_SHORT).show();
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle("Choose a CPU");//sets title on activity.
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//this enables the back button in the toolbar. IMPORTANT, in order for this to work
+        // you must specify android:parentActivityName in the android manifest under the activity you are referencing.
+
+        lowestPriceDisplyTV = (TextView) findViewById(R.id.lowestprice);
+        filterDialog = new Dialog(this);//android context.
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.my_custom_dialog, (ViewGroup) findViewById(R.id.customDialog_root_element));
+
+        filterDialog.setContentView(layout);
+
+        lowestPriceET = (EditText) filterDialog.findViewById(R.id.lowestprice);
+        dismissBTN = (ImageView) filterDialog.findViewById(R.id.closeButton);//the "x" image in the top left
+        dismissBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lowestPriceStr = lowestPriceET.getText().toString();
+                //lowestPriceDisplyTV.setText("Dismissed");
+                //Toast.makeText(CPUActivity.this, lowestPriceET.getText().toString(), Toast.LENGTH_SHORT).show();
+                filterDialog.dismiss();
+            }
+        });
+
+        filterDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+//                lowestPriceET = (EditText)filterDialog.findViewById(R.id.lowestprice);
+//                lowestPriceStr = lowestPriceET.getText().toString();
+                Toast.makeText(CPUActivity.this, lowestPriceET.getText().toString(), Toast.LENGTH_SHORT).show();
+//                lowestPriceDisplyTV.setText(lowestPriceET.getText().toString());
+
+            }
+        });
     }
 
 
