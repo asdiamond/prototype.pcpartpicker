fe78fd413f94dc4230eef78a3672e676e52e8930
Carlos <carloscabada44@gmail.com>
2017-04-28 14:32:36 -0600

    I worked on the Motherboard dialog box exit button.

--- app/src/main/java/com/example/kevinparker/pcpartpicker/MotherboardActivity.java
+++ app/src/main/java/com/example/kevinparker/pcpartpicker/MotherboardActivity.java
@@ -1,13 +1,20 @@
 package com.example.kevinparker.pcpartpicker;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MotherboardActivity extends AppCompatActivity {
+    Dialog motherboardActivityDialog;
+    ImageView closeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,21 @@ public class MotherboardActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Choose a Motherboard");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        motherboardActivityDialog = new Dialog(this);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        View layout =  inflater.inflate(R.layout.motherboard_filter_dialogue,(ViewGroup) findViewById(R.id.motherboardFilterDialogRootElement));
+        motherboardActivityDialog.setContentView(layout);
+
+        closeBtn = (ImageView)motherboardActivityDialog.findViewById(R.id.motherboardcloseButton);
+
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                motherboardActivityDialog.dismiss();
+            }
+        });
+
+
     }
 
     @Override
@@ -30,6 +52,7 @@ public class MotherboardActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.filter_menu_motherboard:
                 Toast.makeText(this, "Filter selected", Toast.LENGTH_SHORT).show();
+                motherboardActivityDialog.show();
                 break;
             case R.id.temp2:
                 Toast.makeText(this, "Temp2 selected", Toast.LENGTH_SHORT).show();
